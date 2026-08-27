4fa3046c0d1f082f50d6b5a9f0cda28ec77711fc
benfgit <benfrost42@icloud.com>
2026-08-27 11:41:00 +0200

    wip

--- scripts/setup/install_llvm.sh
+++ scripts/setup/install_llvm.sh
@@ -1,33 +1,10 @@
 #!/bin/sh
 set -e
 
-VERSION=22.1.7
+LLVM_VERSION=22
 
-cd /usr/local
+curl -fsSL https://apt.llvm.org/llvm.sh | sudo bash -s -- "$LLVM_VERSION" all
 
-ARCHIVE="LLVM-${VERSION}.tar.xz"
-URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/LLVM-${VERSION}-Linux-X64.tar.xz"
+sudo ln -sf /usr/lib/llvm-22/bin/* /usr/local/bin/
 
-echo "Downloading LLVM $VERSION..."
-sudo curl -fsSL -o "$ARCHIVE" "$URL"
-
-echo "Extracting LLVM archive into llvm-${VERSION}/..."
-
-sudo rm -rf "llvm-${VERSION}"
-sudo mkdir -p "llvm-${VERSION}"
-
-# Use `pixz` for parallel decompression
-# It works 2x faster on Github CI, compared to regular `xz`
-sudo tar --use-compress-program=pixz -xf "$ARCHIVE" \
-    -C "llvm-${VERSION}" --strip-components=1
-
-echo "Extracted LLVM archive into llvm-${VERSION}/"
-
-echo "Creating symlinks in /usr/local/bin ..."
-sudo ln -sf /usr/local/llvm-${VERSION}/bin/* /usr/local/bin/
-
-echo "Cleaning up $ARCHIVE..."
-sudo rm -f "$ARCHIVE"
-
-echo "LLVM-${VERSION} installed successfully."
-clang --version
+lldb-dap --version
